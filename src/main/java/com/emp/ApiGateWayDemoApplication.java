5cecdc20c91cb301da7c020bbcc6802a03f0af12
csprashant <prashant.puse@innodeed.com>
2023-11-21 14:31:23 +0530

    initial commit

---
+++ src/main/java/com/emp/ApiGateWayDemoApplication.java
@@ -0,0 +1,15 @@
+package com.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGateWayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWayDemoApplication.class, args);
+	}
+
+}
